09da76239add96768e1d9fec2083854668a25e7d
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2018-02-09 00:44:30 -0500

    Fixed ProPhoto RGB
    they got the value of D50 wrong
    literally just didn't bother to check or whatever
    idk and idc

--- src/colors/rommrgbcolor.rs
+++ src/colors/rommrgbcolor.rs
@@ -78,9 +78,9 @@ impl Color for ROMMRGBColor {
         };   
         // now just apply these in sequence
         ROMMRGBColor{
-            r: clamp(fix_flare(gamma(rr_gg_bb[0]))),
-            g: clamp(fix_flare(gamma(rr_gg_bb[1]))),
-            b: clamp(fix_flare(gamma(rr_gg_bb[2]))),
+            r: fix_flare(gamma(clamp(rr_gg_bb[0]))),
+            g: fix_flare(gamma(clamp(rr_gg_bb[1]))),
+            b: fix_flare(gamma(clamp(rr_gg_bb[2]))),
         }
     }
     /// Converts back from ROMM RGB to XYZ. As ROMM RGB uses D50, any other illuminant given will be
@@ -105,7 +105,7 @@ impl Color for ROMMRGBColor {
         // WolframAlpha is my source for all of the calcluations
         let fix_flare_inv = |x: f64| {
             // fix_flare(2 ^ -9) is cutoff
-            if x >= 0.126246 { // x originally came out of the second part of the cutoff 
+            if x >= 0.005419340625 { // x originally came out of the second part of the cutoff 
                 ((x - 0.003473) / 0.996527).powf(1.0 / 1.8)
             }
             else { // x originally came out of the first part of the cutoff
@@ -118,11 +118,9 @@ impl Color for ROMMRGBColor {
         let r_c = gamma_inv(fix_flare_inv(self.r));
         let g_c = gamma_inv(fix_flare_inv(self.g));
         let b_c = gamma_inv(fix_flare_inv(self.b));
-
         // the standard brilliantly decided to not even bother adding an inverse matrix, but it's
         // best to calculate anyway: it makes it more precise
         let xyz = consts::inv(ROMM()) * Vector3::new(r_c, g_c, b_c);
-        
         // now we convert from D50 to whatever space we need and we're done!
         XYZColor {
             x: xyz[0],
@@ -168,15 +166,29 @@ mod tests {
         assert!((rgb.g - rgb2.g).abs() <= 0.001);
         assert!((rgb.b - rgb2.b).abs() <= 0.001);
     }
+    #[test]
+    fn test_error_accumulation_romm_rgb() {
+        let rgb = ROMMRGBColor{r: 0.6, g: 0.3, b: 0.8};
+        let xyz = rgb.to_xyz(Illuminant::D50);
+        let mut rgb2 = ROMMRGBColor::from_xyz(xyz);
+        for _i in 0..20 {
+            rgb2 = ROMMRGBColor::from_xyz(rgb2.to_xyz(Illuminant::D50));
+            println!("{} {} {}", rgb2.r, rgb2.g, rgb2.b);
+            assert!((rgb.r - rgb2.r).abs() <= 1e-4);
+            assert!((rgb.g - rgb2.g).abs() <= 1e-4);
+            assert!((rgb.b - rgb2.b).abs() <= 1e-4);            
+        }
+    }
         
     #[test]
     fn test_romm_rgb_xyz_conversion_with_gamut() {
         let wp = Illuminant::D65.white_point();
         let xyz = XYZColor{x: wp[0], y: wp[1], z: wp[2], illuminant: Illuminant::D65};
         let rgb: ROMMRGBColor = ROMMRGBColor::from_xyz(xyz);
-        let xyz2: XYZColor = rgb.to_xyz(Illuminant::D65);
+        let xyz2 = rgb.to_xyz(Illuminant::D65);
+        println!("{} {} {}", rgb.r, rgb.g, rgb.b);
         println!("{} {} {} {} {} {}", xyz.x, xyz.y, xyz.z, xyz2.x, xyz2.y, xyz2.z);
-        assert!(xyz.approx_equal(&xyz2));
+        assert!(xyz.approx_visually_equal(&xyz2));
     }
 
     #[test]
