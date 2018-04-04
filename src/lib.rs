58ed12f23eee283a7db1f58aa8220ad372046f10
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2018-04-04 00:41:58 -0400

    Trying to fix docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //! working with color convenient enough that it's *easier* to treat colors correctly than it is to do
 //! anything else.
 
-#![doc(html_root_url = "https://docs.rs/scarlet/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/scarlet/0.2.1")]
 
 // we don't mess around with documentation
 #![deny(missing_docs)]
